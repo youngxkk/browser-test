f592b76401ffd9e31ce0103acd9c1698fa013d1e
youngxkk <36769052@qq.com>
2018-06-09 18:04:40 +0800

    first

--- browser/ViewController.swift
+++ browser/ViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelText: UILabel!
 
+    @IBAction func button(_ sender: UIButton) {
+        print("test")
+        
+        labelText.text = "test"
+        
+        //设置你要浏览的网页地址
+        let urll = NSURL(string: "http://www.apple.com");
+        
+        let requestt = NSURLRequest(url: urll! as URL);
+        webView.loadRequest(requestt as URLRequest)
+        
+    }
+        //重要，需要打开info.plist文件,添加字典App Transport Security Settings，并在其下增加一个Bool类型的键Allow Arbitrary Loads，其值设置为YES，只有这样才行。
+
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
