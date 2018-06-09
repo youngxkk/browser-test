de0318b666f5f921b78952f9e0984c92fa2e5838
youngxkk <36769052@qq.com>
2018-06-09 10:42:56 +0800

    Initial Commit

---
+++ browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  browser
+//
+//  Created by youngxkk on 2018/6/9.
+//  Copyright © 2018 youngxkk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
